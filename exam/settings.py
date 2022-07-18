6d825d3069adc2ab3ed4d5209004457e4db0f94c
Yusuf <yustaoab@gmail.com>
2022-07-18 18:42:43 +0100

    k

--- exam/settings.py
+++ exam/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'online_exam',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
